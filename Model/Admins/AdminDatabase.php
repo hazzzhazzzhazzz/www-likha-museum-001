eba594a4884d665ebc3169b61430afb6d1aa9d50
Hazmene Andrea Taguinod <hazmeneandrea.taguinod.cics@ust.edu.ph>
2026-05-23 18:35:39 +0800

    changed to port 3307

--- Model/Admins/AdminDatabase.php
+++ Model/Admins/AdminDatabase.php
@@ -2,8 +2,8 @@
 
 class AdminDatabase {
     private $host = "127.0.0.1";
-    private $port = "3309";
-    // naka-3306 in default pero naka-3309 sa HeidiSQL and xampp ko (because ang port 3306 is being used by another application for ICS2607 si Workbench)
+    private $port = "3307";
+    // naka-3307 in default pero naka-3309 sa HeidiSQL and xampp ko (because ang port 3306 is being used by another application for ICS2607 si Workbench)
     private $DBname = "employees-admins_db";
     private $username = "root";
     private $password = "";
